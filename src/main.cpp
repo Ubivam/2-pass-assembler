02ca020c10a52683fcf900a339437c252cde491a
Ubivam <strahinja.janjic@yahoo.com>
2019-08-20 17:20:29 +0200

    utility

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,18 @@ int main(int argc, char *argv[])
         if (!Utility::readFile(argv[1], instructions))
         {
                 PRINT("There was an error in readingFile");
-                return = -1;
+                return -1;
         }
         //TODO: Operation Table initialize
         if (!Assembler::firstPass(instructions))
         {
                 PRINT("There was an error with first Pass");
-                return = -1;
+                return -1;
         }
         if (!Assembler::secoundPass(instructions))
         {
                 PRINT("There was an error with secound Pass");
-                return = -1;
+                return -1;
         }
         Utility::writeFile(argv[2]);
         PRINT("Both passes succesfully passed!");
--- src/utility.cpp
+++ src/utility.cpp
@@ -30,8 +30,16 @@ bool Utility::readFile(std::string fileName, ArrayOfStrings &instructions)
 std::vector<std::string> Utility::tokenizeString(const std::string &line, const std::string &delimetar)
 {
     std::vector<std::string> tokens;
-    if(str.empty())return tokens;
+    if (line.empty())
+        return tokens;
     //Preskoci prvi delimetar na pocetku
-    std::string::size_type lastPos = line.find_first_not_of(delimetar,0);
-    std::string::size_type pos = line.find_first_of(delimetar,lastPos);
+    std::string::size_type lastPos = line.find_first_not_of(delimetar, 0);
+    std::string::size_type pos = line.find_first_of(delimetar, lastPos);
+
+    // std::string::npos means unitl the end of the string
+    while (std::string::npos != pos || std::string::npos != lastPos)
+    {
+        std::string subSt= subSt.substr(latest, pos - lastPos);
+        if(subSt.find())
+    }
 }
